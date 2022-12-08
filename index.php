23ac16c0cfbc0027c2b8dcce049dbb9770d434bc
Imonitie Aregbeyen <aregbeyenimonitie@gmail.com>
2022-12-08 10:35:33 +0100

    added contact form

--- index.php
+++ index.php
@@ -16,6 +16,15 @@ $hero_p = 'Our team will identify a custom solution to fit your idea, timeline, 
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= ucwords($title) ?></title>
   <link rel="stylesheet" href="css/bootstrap.min.css">
+  <style>
+    .bg-green {
+      background-color: #008037;
+      color: #fff;
+    }
+    .bg-img {
+      background-image: url('img/shubham-dhage.jpg');
+    }
+  </style>
 </head>
 <body>
   <nav class="navbar sticky-top navbar-expand-lg bg-light">
@@ -41,17 +50,47 @@ $hero_p = 'Our team will identify a custom solution to fit your idea, timeline, 
     </div>
   </nav>
 
-  <div class="bg-dark text-secondary py-5 text-center">
+  <div class="bg-dark text-secondary py-5 text-center bg-img">
     <div class="container py-5">
       <h1 class="display-4 text-white"><?= strtoupper($hero_h) ?></h1>
       <div class="col-lg-10 mx-auto">
-        <p class="lead mb-4"><?= $hero_p ?></p>
+        <p class="lead text-white mb-4"><?= $hero_p ?></p>
         <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-          <a href="" class="btn btn-dark btn-lg px-5">&darr;</a>
+          <a href="#contact-us" class="btn btn-outline-light btn-lg px-4 rounded-0">
+            <span>GET STARTED</span> <span>&rarr;</span>
+          </a>
         </div>
       </div>
     </div>
   </div>
+
+  <section id="contact-us" class="bg-green py-5">
+    <div class="container py-5">
+      <h2 class="display-5">LET'S WORK ON YOUR PROJECT</h2>
+      <div class="border-bottom border-5 border-primary" style="width:64px"></div>
+      <p class="lead">Fill and send the following form and we will contact shortly</p>
+      <form>
+        <div class="row">
+          <div class="col-lg">
+            <div class="mb-3">
+              <input name="name" type="text" class="form-control rounded-0 border border-4" placeholder="Your name" required>
+            </div>
+          </div>
+          <div class="col-lg">
+            <div class="mb-3">
+              <input name="phone" type="tel" class="form-control rounded-0 border border-4" placeholder="Phone number" required>
+            </div>
+          </div>
+        </div>
+        <div class="mb-3">
+          <textarea name="work" placeholder="Project description" rows="4" class="form-control rounded-0 border border-4"></textarea>
+        </div>
+        <div class="d-flex justify-content-end d-lg-block">
+          <button type="submit" class="btn btn-primary rounded-0 px-4">SEND</button>
+        </div>
+      </form>
+    </div>
+  </section>
 
   <script src="js/bootstrap.bundle.min.js"></script>
 </body>
